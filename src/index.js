64ce530c7e4f14b82f6213bc661e44a70882071e
kukikoe <ja.bondareva2014@yandex.ru>
2018-07-11 23:58:51 +0300

    add authorization and registration popups

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import './main.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Router, Route, browserHistory} from 'react-router'
+import {Provider} from 'react-redux'
+
+import reducers from 'reducers'
+import Layout from 'containers/layout'
+import Book from 'containers/book'
+import Books from 'containers/books'
+import Basket from 'containers/basket'
+
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(thunk)
+));
+
+const  history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render (
+    <Provider store={store}>
+         <Router history={history}>
+            <Route component={Layout}>
+                <Route  path='/' component={Book}/>
+                <Route path='categories/:id' component={Book} />
+            </Route>
+             <Route path='book/:id' component={Books} />
+             <Route path='/basket' component={Basket} />
+         </Router>
+    </Provider>,
+    document.getElementById('root')
+);
